e7204bd92673446490e79035d5b1fb59a0b9c4c4
Luqmanul Hakim <luqman@konsultanku.co.id>
2021-01-12 15:06:27 +0700

    add file

---
+++ groupid1.js
@@ -0,0 +1,11 @@
+/*
+"Ingatlah, laknat Allah (ditimpakan) atas orang-orang yang zalim" (QS. Hud: 18).
+*/
+let arrWEB = [
+  'chartpedia.blogspot.com',
+  'koloniadventure.blogspot.com',
+  'buffetsclothing.blogspot.com',
+  '192.168.20.220',
+  'creatblogstore.blogspot.com',
+  'ashiaaap-tasikmalaya.blogspot.com',
+];
---
+++ groupid2.js
@@ -0,0 +1,1 @@
+let arrWEB = ['192.168.20.220','siplah.barungoding.com','lapak.ardesign.web.id','www.m.ardesign.web.id',];
